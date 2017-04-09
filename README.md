65684b72cbf9e02c0df6dcd41d2f72b939e88cd0
yanying <yanyinghq@163.com>
2017-04-09 22:44:05 +0800

    修改演示页面和文档

--- README.md
+++ README.md
@@ -1,2 +1,117 @@
 # PaginationY
-可以灵活定制的PHP分页类
+
+PaginationY是一个可以灵活定制的PHP分页类。具有以下优点：
+
+ 1. 可以灵活的自定义配置各项参数
+ 2. 灵活的配置输出的分页链接类别
+ 3. 灵活的定制分页按钮的样式
+ 4. 可以定制URL中获取分页值的参数名称
+ 5. 可以适配原始链接带参数或者不带参数
+ 6. 自由定制链接按钮显示文字。如：上一页、下一页
+ 7. 可以自由定制分页链接div的类名
+
+ ## 使用方法
+
+ 引入该`Pagination.php`
+
+```php
+require_once('Pagination.php');
+```
+
+
+
+实例化类，传入参数
+
+```php
+$param = array('totalRows'=>'200');
+$page1 = new Pagination($param);
+```
+
+
+
+创建连接，并且输出
+
+```php
+echo $page1->pagination();
+```
+
+
+
+## 参数
+
+参数在类初始化的时候传入，类型必须为数组。例如：
+
+```php
+$page1 = new Pagination(array(
+    'totalRows'=>'200',
+    'pageSize'=>'20',
+    'offset'=>'5',
+    'pageParam'=>'page',
+    'activeClassName'=>'active',
+    'indexPageLabel'=>'首页',
+    'prevPageLabel'=>'上一页',
+    'nextPageLabel'=>'下一页',
+    'endPageLabel'=>'尾页',
+    'class'=>'page'
+));
+```
+
+
+
+###  totalRows
+
+其中，`totalRows`为必填参数，含义为数据库检索出来的所有数据总数。
+
+
+
+### pageSize
+
+选填参数，每一页显示的记录数量。默认为20
+
+
+
+### offset
+
+选填参数，页码的左右偏移量。假设当前页码为5，则在5的左右各显示几个数字链接，默认为4个，则效果为1,2,3,4,5,6,7,8,9
+
+
+
+### pageParam
+
+选填参数，URL中当前页码的参数名称。通过$_GET['page']获取当前页码时候的名字，默认为page。
+
+
+
+### activeClassName
+
+选填参数，当前页码高亮标签的类名，默认是`active`
+
+
+
+### indexPageLabel
+
+选填参数，首页链接按钮显示名称。默认为首页
+
+
+
+### prevPageLabel
+
+选填参数，上一页链接按钮显示名称。默认为上一页
+
+
+
+### nextPageLabel
+
+选填参数，下一页链接按钮显示名称。默认为下一页
+
+
+
+### endPageLabel
+
+选填参数，尾页链接按钮显示名称。默认为尾页
+
+
+
+### class
+
+选填参数，所有分页链接按钮最外层div的类名。默认为`pagination`
--- index.php
+++ index.php
@@ -1,9 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>PaginationY - 示例演示_星空幻颖</title>
+    <style type="text/css">
+        .pagination{ overflow: hidden; margin-top:20px;}
+        .pagination a{display:block; height:30px; min-width:30px; text-align:center; font-size:14px; border:1px solid #d6d6d6; float:left; margin-left:10px; padding:3px 5px; line-height:30px; text-decoration:none; color:#666;}
+        .pagination a:hover,
+        .pagination a.active{background:#FF4500; border-color:#FF4500; color:#FFF;}
+    </style>
+</head>
+
+<body>
+
 <?php
+require_once('Pagination.php');
 
-require_once 'page.php';
-header('content-type:text/html;charset=utf-8');
-$obj = new Pagination(['totalRows'=>'200','class'=>'pagination box']);
-echo '<style type="text/css">.active{color:red}</style>';
+$param = array(
+    'totalRows'=>'200',
+    'pageSize'=>'20',
+    'offset'=>'5',
+    'pageParam'=>'page',
+    'activeClassName'=>'active',
+    'indexPageLabel'=>'首页',
+    'prevPageLabel'=>'上一页',
+    'nextPageLabel'=>'下一页',
+    'endPageLabel'=>'尾页',
+    'class'=>'page'
+);
 
-echo $obj->pagination();
+$page1 = new Pagination($param);
+$page2 = new Pagination($param);
+$page3 = new Pagination($param);
+$page4 = new Pagination($param);
+$page5 = new Pagination($param);
 
+echo '总记录数：100';
+echo '<hr />';
+echo '每页记录2条<hr/ >';
+echo '当前页码：'.$page1->getCurrentPage().'<hr />';
+echo '共计'.$page1->getPageAmount().'页<hr />';
+echo $page1->pagination();
+echo $page2->pagination('1'); //默认为1，所以和不填写效果一样
+echo $page3->pagination('2');
+echo $page4->pagination('3');
+echo $page5->pagination('4');
+?>
+</body>
+</html>
